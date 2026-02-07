5f1c728f82b523695852eed7c6f16185cfad3b81
Rodrigo <rchaves@uchicago.edu>
2026-02-07 17:26:23 -0600

    Merged Zillow Data to Energy Data

--- R Code/JoinZillowEnergy.r
+++ R Code/JoinZillowEnergy.r
@@ -2,26 +2,24 @@ library(tidyverse)
 library(sf)
 library(stringr)
 
-
-
+# Use relative paths so your teammates can run this too!
 zillow <- read.csv("data/zillow_annual_summary.csv")
-
 
 zillow <- zillow %>% 
   filter(year >= 2021 & year < 2025)
 
+# If the energy files are inside your repo's data folder:
+folder_path <- "data/" 
+# OR if you must point to your Windows Downloads folder from WSL:
+# folder_path <- "/mnt/c/Users/rodri/Downloads/EnergyData/"
 
-folder_path <- "C:/Users/rodri/Downloads/EnergyData/"
-files <- list.files(path = folder_path, pattern = "*.csv", full.names = TRUE)
+files <- list.files(path = folder_path, pattern = "iou_zipcodes_\\d{4}\\.csv", full.names = TRUE)
 
 energy <- read_csv(files, id = "file_source")
 
 energy <- energy %>%
-  mutate(year = as.numeric(str_extract(file_source, "\\d{4}"))) %>%
-  # Standardize zip to character to avoid leading zero issues (e.g., 02108)
-  mutate(zip = as.character(zip))
-
-
+  mutate(year = as.numeric(str_extract(file_source, "\\d{4}")),
+         zip = as.character(zip))
 
 zillow <- zillow %>%
   mutate(
@@ -29,10 +27,8 @@ zillow <- zillow %>%
     year = as.numeric(year)
   )
 
-
 final_df <- inner_join(zillow, energy, 
                        by = c("RegionName" = "zip", "year" = "year"))
 
-
-
-write.csv(final_df, row.names = FALSE, col.names = FALSE, "C:/Users/rodri/Desktop/UChicago/2.SecondQuarter/Computer Science with Applications 2/Project/ZillowEnergy.csv")
+# Saving to the project data folder instead of a local Desktop path
+write.csv(final_df, "data/ZillowEnergy.csv", row.names = FALSE)
