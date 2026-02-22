bb80eb2e2a99bfd8163a392c004fe7d0b0d55d52
Carlos Eduardo Vargas <cev@uchicago.edu>
2026-02-21 21:44:55 -0600

    added a csv with chicago data centers and its python code

--- python_scripts/chicago_data_centers.py
+++ python_scripts/chicago_data_centers.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Loading the dataset:
-datacenters = pd.read_csv("/mnt/data/top_us_cities_datacenters.csv")
+datacenters = pd.read_csv("data/top_us_cities_datacenters.csv")
 
 # Filtering only rows where scraped_city is Chicago:
 chicago_dc = datacenters[datacenters["scraped_city"] == "Chicago"].copy()
@@ -10,7 +10,7 @@ chicago_dc = datacenters[datacenters["scraped_city"] == "Chicago"].copy()
 chicago_dc["first_permit"] = ""
 
 # Saving to a new CSV file
-chicago_dc.to_csv("chicago_data_centers.csv", index=False)
+chicago_dc.to_csv("data/chicago_data_centers.csv", index=False)
 
 # Printing result:
 print(chicago_dc.head())
