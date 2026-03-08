b60185558dba6ade15a3b5c8c716fbff1b2f8407
StLaurentMTL <lburton12@uchicago.edu>
2026-03-07 21:11:10 -0600

    Adding tests for index

--- data_analysis/index.py
+++ data_analysis/index.py
@@ -1,34 +1,16 @@
 import pandas as pd
 from pathlib import Path
 
-# NOTE: THE METHODOLOGY AND INPUTS ARE SUBJECT TO CHANGE AS DATA BECOMES CLEANER
 
 # Data Analysis
-# We will create index as an indicator of affordability. Zip codes with
-# more data centers, higher average electricity and water prices, and
-# home prices will have a higher index, and thus be less affordable and more
-# at risk of pricing out residents of the region
+# We will create index as the impact on housing and cost of living prices
+# following building of a datacenter within the zipcode. The index will
+# be constructed using the change in housing prices and cost of living
+# prices pre-data center permit and post-data center permitting.
 
-# Importing our MOCK DATA as a pandas dataframe
 
-FILEPATH = Path(__file__).parent / "top_10_us_cities_datacenters_augmented_mock.csv"
-OUTPATH = Path(__file__).parent / "top_10_us_cities_datacenters_scores_mock.csv"
-
-data_centers = pd.read_csv(FILEPATH)
-
-# # Calculating the unique counts of data centers by zip codes
-# unique_data_centers = (
-#     data_centers.groupby("zip_code")["facility"]
-#     .nunique()
-#     .reset_index()
-#     .rename(columns={"facility": "facility_count"})
-# )
-
-# # Left joining this onto data_centers dataset
-# data_centers = pd.merge(data_centers, unique_data_centers, on="zip_code")
-
-# SCORING 
-def scoring(dataset,variable,method = "composite"):
+# SCORING
+def scoring(dataset, variable, method="composite"):
     """
     Takes a variable and calculates a "ranking" based on its decile value or its
     corresponding z-score. Each method has their distinct advantages and disadvantages
@@ -37,6 +19,9 @@ def scoring(dataset,variable,method = "composite"):
     Inputs:
            variable: a pandas dataframe variable by which we are calculating scores for
     """
+    if method not in {"composite", "z-score"}:
+        raise ValueError("Method must be 'composite' or 'z-score'!")
+
     if method == "composite":
         score_name = f"{variable}_score"
 
@@ -45,74 +30,33 @@ def scoring(dataset,variable,method = "composite"):
     elif method == "z-score":
         z_score_name = f"{variable}_z_score"
 
-        dataset[z_score_name] = (dataset[variable] - dataset[variable].mean()) / dataset[variable].std()
-    
-    else:
-        raise ValueError("Method must be 'composite' or 'z-score'!")
+        dataset[z_score_name] = (
+            dataset[variable] - dataset[variable].mean()
+        ) / dataset[variable].std()
 
-# # Composite Scoring Method
-# scoring("avg_electricity_cost_usd_per_kwh", method = "composite")
-# scoring("avg_home_price_usd",method = "composite")
-# scoring("avg_water_cost_usd_monthly",method = "composite")
 
-# # Z-Scoring Method
-# scoring("avg_electricity_cost_usd_per_kwh",method = "z-score")
-# scoring("avg_home_price_usd",method = "z-score")
-# scoring("avg_water_cost_usd_monthly",method = "z-score")
+# Creating the index score based on weights. The weights MUST add to 1!
+def index(dataset, housing_weight=0.50, HC_weight=0.50, method="composite"):
+    """
+    Creates a composite index score for a zip code based on selected weights
 
-# # Creating the index score based on weights. The weights MUST add to 1!
-# def index(
-#     facility_weight=0.25, home_weight=0.25, electricity_weight=0.25, water_weight=0.25
-# ,method = "composite"):
-#     """
-#     Creates a composite index score for a zip code based on selected weights
+    Inputs:
+           facility_weight: The weight assigned to number of data center facilities
+           home_weight: The weight assigned to the ranking of average housing costs
+           electricity_weight: The weight assigned to the ranking of electricity costs
+           water_weight: The weight assigned to the ranking of water usage costs
+    """
+    if housing_weight + HC_weight != 1:
+        raise ValueError("Weights must add up 1")
 
-#     Inputs:
-#            facility_weight: The weight assigned to number of data center facilities
-#            home_weight: The weight assigned to the ranking of average housing costs
-#            electricity_weight: The weight assigned to the ranking of electricity costs
-#            water_weight: The weight assigned to the ranking of water usage costs
-#     """
-#     if facility_weight + home_weight + electricity_weight + water_weight != 1:
-#         raise ValueError("Weights must add up 1")
+    weights = {"housing weight": housing_weight, "cost of living weight": HC_weight}
 
-#     weights = {
-#         "facility_weight": facility_weight,
-#         "home_price_weight": home_weight,
-#         "electricity_price_weight": electricity_weight,
-#         "water_price_weight": water_weight,
-#     }
+    if method == "composite":
+        return (dataset["HC_Score_Change_score"] * weights["cost of living weight"]) + (
+            dataset["Housing_Change_score"] * weights["housing weight"]
+        )
 
-#     if method == "composite":
-#         data_centers["index_score"] = (
-#             (data_centers["facility_count"] * weights["facility_weight"])
-#             + (
-#                 data_centers["avg_electricity_cost_usd_per_kwh_score"]
-#                 * weights["electricity_price_weight"]
-#             )
-#             + (data_centers["avg_home_price_usd_score"] * weights["home_price_weight"])
-#             + (
-#                 data_centers["avg_water_cost_usd_monthly_score"]
-#                 * weights["water_price_weight"]
-#             )
-#         )
-#     elif method == "z-score":
-#                 data_centers["z_index_score"] = (
-#             (data_centers["facility_count"] * weights["facility_weight"])
-#             + (
-#                 data_centers["avg_electricity_cost_usd_per_kwh_z_score"]
-#                 * weights["electricity_price_weight"]
-#             )
-#             + (data_centers["avg_home_price_usd_z_score"] * weights["home_price_weight"])
-#             + (
-#                 data_centers["avg_water_cost_usd_monthly_z_score"]
-#                 * weights["water_price_weight"]
-#             )
-#         )
-
-# # Running index:
-# index()
-
-# #Exporting analysis onto .csv file
-# #Composite Index mock output
-# data_centers.to_csv(OUTPATH)
+    elif method == "z-score":
+        return (
+            dataset["HC_Score_Change_z_score"] * weights["cost of living weight"]
+        ) + (dataset["Housing_Change_z_score"] * weights["housing weight"])
--- data_analysis/test_index.py
+++ data_analysis/test_index.py
@@ -7,62 +7,64 @@ from index import scoring
 OUTPATH = Path(__file__).parent / "tests_index_dummy_data.csv"
 SEED = 2006
 
+
 # Setting fixture dataset
 def make_data(n):
-
     # Setting the seed
     Faker.seed(SEED)
     random.seed(SEED)
 
-    #Settign Minimum Bounds
+    # Settign Minimum Bounds
     MIN_HOME_PRICE = 100000
     MIN_COL_PRICE = 100
 
-    #Setting Maximum Bounds
+    # Setting Maximum Bounds
     MAX_HOME_PRICE = 1000000
     MIN_MAX_PRICE = 10000
 
-    #Setting fake dataset 
+    # Setting fake dataset
     dummy_data = Faker(["en_US"])
     names = [dummy_data.company() for _ in range(n)]
     zipcodes = [dummy_data.zipcode() for _ in range(n)]
-    home_price_change = [random.randint(MIN_HOME_PRICE,MAX_HOME_PRICE) for _ in range(n)]
-    cost_of_living_change = [random.randint(MIN_COL_PRICE,MAX_HOME_PRICE) for _ in range(n)]
+    home_price_change = [
+        random.randint(MIN_HOME_PRICE, MAX_HOME_PRICE) for _ in range(n)
+    ]
+    cost_of_living_change = [
+        random.randint(MIN_COL_PRICE, MAX_HOME_PRICE) for _ in range(n)
+    ]
 
-    testing_data = pd.DataFrame({"Name":names,
-                                 "Zip Code":zipcodes,
-                                 "Home Price Change":home_price_change,
-                                 "Cost of Living Change":cost_of_living_change})
+    testing_data = pd.DataFrame(
+        {
+            "Name": names,
+            "Zip Code": zipcodes,
+            "Home Price Change": home_price_change,
+            "Cost of Living Change": cost_of_living_change,
+        }
+    )
 
     return testing_data
+
 
 dummy_set = make_data(20)
 # dummy_set.to_csv(OUTPATH)
 
+
 # Testing scoring()
 def test_scoring_composite():
-
-    #Creating Dataset
+    # Creating Dataset
     dummy_set = make_data(20)
-    
-    #Scoring
-    scoring(dummy_set,"Home Price Change")
 
-    #Testing scores for Home Price Changes
+    # Scoring
+    scoring(dummy_set, "Home Price Change")
+
+    # Testing scores for Home Price Changes
     assert dummy_set.iloc[0]["Home Price Change_score"] == 7
     assert dummy_set.iloc[1]["Home Price Change_score"] == 5
     assert dummy_set.iloc[4]["Home Price Change_score"] == 1
     assert dummy_set.iloc[19]["Home Price Change_score"] == 6
 
-    #Testing scores for Cost of Living Changes
-    scoring(dummy_set,"Cost of Living Change")
+    # Testing scores for Cost of Living Changes
+    scoring(dummy_set, "Cost of Living Change")
+
 
 test_scoring_composite()
-
-
-
-
-
-
-
-    
